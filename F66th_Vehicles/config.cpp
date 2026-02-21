808b5383a283f7126f8144d2202f738deaab81d2
PaladinPebbles <dakotasthilaire@gmail.com>
2026-02-20 21:11:04 -0500

    Arty go BRRR
    Tonk update

--- F66th_Vehicles/config.cpp
+++ F66th_Vehicles/config.cpp
@@ -30,6 +30,7 @@ class CfgPatches
 
 class CfgVehicles
 {
+	class textureSources;
 	//Springbok
 
 	class DMNS_M511_Springbok_APC;
@@ -538,6 +539,68 @@ class CfgVehicles
 		armor = 1275;
 	};
 
+	//Basilisk
+	class B_MBT_01_arty_F;
+	class PB_Basilisk_155mm : B_MBT_01_arty_F
+	{
+		author = "[F66th] Pebbles";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "[F66th] Basilisk";
+		faction = "PB_Bluefor_Faction";
+		crew = "OPTRE_UNSC_Marine_Soldier_Crewman";
+		side = 1;
+		editorCategory = "PB_Bluefor_F";
+		editorSubcategory = "EdSubcat_Artillery";
+		vehicleClass = "Armored";
+		ace_hunterkiller[] = { {{0},3},{{0,0},3} };
+		
+		weapons[] = { "PB_AMOS_155mm" };
+
+		hasDriver = -1;
+
+		forceInGarage = 1;
+
+		class TransportWeapons
+		{
+			delete _xx_arifle_MX_khk_F;
+		};
+		class TransportMagazines
+		{
+			delete _xx_30Rnd_65x39_caseless_mag;
+			class _xx_OPTRE_60Rnd_762x51_Mag_AP
+			{
+				magazine = "OPTRE_60Rnd_762x51_Mag_AP";
+				count = 5;
+			};
+		};
+		class TransportItems
+		{
+			delete _xx_FirstAidKit;
+			class _xx_Toolkit
+			{
+				name = "Toolkit";
+				count = 1;
+			};
+			class _xx_MEU_Biofoam_Light
+			{
+				name = "MEU_Biofoam_Light";
+				count = 5;
+			};
+		};
+		hiddenSelections[] = { "Camo1","Camo2","Camo3","CamoNet" };
+		hiddenSelectionsTextures[] = {
+			"\F66th_Vehicles\data\Basilisk\Basilisk_Body.paa",
+			"\F66th_Vehicles\data\Basilisk\Basilisk_155mm.paa",
+			"\F66th_Vehicles\data\Basilisk\Basilisk_AGL",
+			"\F66th_Vehicles\data\Basilisk\camonet_greenbeige_co.paa"
+		};
+		textureList[] = {"color66th",1};
+
+	};
+
+
+
 
 };
 
@@ -552,9 +615,206 @@ class CfgRecoils
 	};
 };
 class cfgweapons {
+	//mortar
+	class Mode_SemiAuto
+	{
+		sounds[] = { "StandardSound","SilencedSound" };
+		displayName = "$STR_DN_MODE_SEMIAUTO";
+		textureType = "semi";
+		recoil = "recoil_single_primary_3outof10";
+		recoilProne = "recoil_single_primary_prone_3outof10";
+		aiDispersionCoefY = 1.7;
+		aiDispersionCoefX = 1.4;
+		soundBurst = 0;
+		requiredOpticType = -1;
+		aiRateOfFire = 2.0;
+		aiRateOfFireDispersion = 1;
+		aiRateOfFireDistance = 500;
+	};
+	class Mode_Burst : Mode_SemiAuto
+	{
+		displayName = "$STR_DN_MODE_BURST";
+		textureType = "burst";
+		recoil = "recoil_auto_primary_3outof10";
+		recoilProne = "recoil_auto_primary_prone_3outof10";
+		aiDispersionCoefY = 2.4;
+		aiDispersionCoefX = 1.9;
+		soundBurst = 0;
+	};
+	class CannonCore;
+	class LauncherCore;
+	//155mm
+	class PB_AMOS_155mm : CannonCore
+	{
+		scope = 1;
+		displayName = "PB_mortar_155mm_AMOS0";
+		nameSound = "cannon";
+		cursor = "mortar";
+		cursorAim = "EmptyCursor";
+		sounds[] = { "StandardSound" };
+		class StandardSound
+		{
+			begin1[] = { "A3\Sounds_F\arsenal\weapons_vehicles\cannon_155mm\sochor_155mm_distant",2.5118864,1,1500 };
+			soundBegin[] = { "begin1",1 };
+		};
+		reloadSound[] = { "A3\Sounds_F\vehicles\armor\noises\reload_tank_cannon_2",31.622778,1,15 };
+		reloadTime = 1;
+		magazineReloadTime = 3;
+		autoReload = 1;
+		canLock = 0;
+		magazines[] = { "32Rnd_155mm_Mo_shells","32Rnd_155mm_Mo_shells_O","6Rnd_155mm_Mo_smoke","6Rnd_155mm_Mo_smoke_O","2Rnd_155mm_Mo_guided","4Rnd_155mm_Mo_guided","4Rnd_155mm_Mo_guided_O","2Rnd_155mm_Mo_LG","4Rnd_155mm_Mo_LG","4Rnd_155mm_Mo_LG_O","6Rnd_155mm_Mo_mine","6Rnd_155mm_Mo_mine_O","2Rnd_155mm_Mo_Cluster","2Rnd_155mm_Mo_Cluster_O","6Rnd_155mm_Mo_AT_mine","6Rnd_155mm_Mo_AT_mine_O" };
+		modes[] = { "Single1","Single2","Single3","Single4","Single5","Burst1","Burst2","Burst3","Burst4","Burst5" };
+		class GunParticles
+		{
+			class Effect1
+			{
+				effectName = "ArtilleryFired1";
+				positionName = "Usti hlavne3";
+				directionName = "Konec hlavne3";
+			};
+			class Effect2
+			{
+				effectName = "ArtilleryFiredL";
+				positionName = "Usti hlavne";
+				directionName = "Usti hlavne b";
+			};
+			class Effect3
+			{
+				effectName = "ArtilleryFiredR";
+				positionName = "Usti hlavne";
+				directionName = "Usti hlavne b";
+			};
+		};
+		class Single1 : Mode_SemiAuto
+		{
+			displayName = "$STR_A3_mortar_120mm_AMOS_Single10";
+			sounds[] = { "StandardSound" };
+			class StandardSound
+			{
+				begin1[] = { "A3\Sounds_F\arsenal\weapons_vehicles\cannon_155mm\sochor_155mm_distant",2.5118864,1,1500 };
+				soundBegin[] = { "begin1",1 };
+			};
+			reloadSound[] = { "A3\sounds_f\dummysound",1.0,1,20 };
+			reloadTime = 1;
+			artilleryDispersion = 3.2;
+			artilleryCharge = 0.19;
+			aiRateOfFire = 1;
+			aiRateOfFireDistance = 10;
+			minRange = 0;
+			minRangeProbab = 0.01;
+			midRange = 1;
+			midRangeProbab = 0.01;
+			maxRange = 200000;
+			maxRangeProbab = 0.01;
+		};
+		class Single2 : Single1
+		{
+			displayName = "$STR_A3_mortar_120mm_AMOS_Single20";
+			artilleryCharge = 0.3;
+		};
+		class Single3 : Single1
+		{
+			displayName = "$STR_A3_mortar_120mm_AMOS_Single30";
+			artilleryCharge = 0.48;
+		};
+		class Single4 : Single1
+		{
+			displayName = "$STR_A3_mortar_120mm_AMOS_Single40";
+			artilleryCharge = 0.8;
+		};
+		class Single5 : Single1
+		{
+			displayName = "$STR_A3_mortar_120mm_AMOS_Single50";
+			artilleryCharge = 1;
+		};
+		class Burst1 : Mode_Burst
+		{
+			showToPlayer = 1;
+			displayName = "$STR_A3_mortar_120mm_AMOS_Burst10";
+			burst = 6;
+			sounds[] = { "StandardSound" };
+			class StandardSound
+			{
+				begin1[] = { "A3\Sounds_F\arsenal\weapons_vehicles\cannon_155mm\sochor_155mm_distant",2.5118864,1,1500 };
+				soundBegin[] = { "begin1",1 };
+			};
+			reloadSound[] = { "A3\sounds_f\dummysound",1.0,1,20 };
+			soundBurst = 0;
+			reloadTime = 3;
+			minRange = 100;
+			minRangeProbab = 0.5;
+			midRange = 1500;
+			midRangeProbab = 0.7;
+			maxRange = 20000;
+			maxRangeProbab = 0.5;
+			artilleryDispersion = 5.3;
+			artilleryCharge = 0.19;
+		};
+		class Burst2 : Burst1
+		{
+			showToPlayer = 0;
+			displayName = "$STR_A3_mortar_120mm_AMOS_Burst20";
+			minRange = 2000;
+			minRangeProbab = 0.4;
+			midRange = 3000;
+			midRangeProbab = 0.6;
+			maxRange = 5200;
+			maxRangeProbab = 0.4;
+			artilleryCharge = 0.3;
+		};
+		class Burst3 : Burst1
+		{
+			showToPlayer = 0;
+			displayName = "$STR_A3_mortar_120mm_AMOS_Burst30";
+			minRange = 5200;
+			minRangeProbab = 0.3;
+			midRange = 8000;
+			midRangeProbab = 0.4;
+			maxRange = 13300;
+			maxRangeProbab = 0.3;
+			artilleryCharge = 0.48;
+		};
+		class Burst4 : Burst1
+		{
+			showToPlayer = 0;
+			displayName = "$STR_A3_mortar_120mm_AMOS_Burst40";
+			minRange = 14600;
+			minRangeProbab = 0.2;
+			midRange = 25000;
+			midRangeProbab = 0.3;
+			maxRange = 37000;
+			maxRangeProbab = 0.2;
+			artilleryCharge = 0.8;
+		};
+		class Burst5 : Burst1
+		{
+			showToPlayer = 0;
+			displayName = "$STR_A3_mortar_120mm_AMOS_Burst50";
+			minRange = 25000;
+			minRangeProbab = 0.1;
+			midRange = 40000;
+			midRangeProbab = 0.2;
+			maxRange = 58000;
+			maxRangeProbab = 0.1;
+			artilleryCharge = 1;
+		};
+	};
+
+
+
+
 	class DMNS_105mm_M511;
 	class PB_YeetCannon : DMNS_105mm_M511
 	{
 		recoil = "funny_recoil";
 	};
+
+
+
+
+
+
+
+
+
 };
